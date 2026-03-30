5db474d504a7ec2c4bd4ae3be5a05956a12f667d
yasmin-souza21 <YasminSouza21@users.noreply.github.com>
2026-03-30 18:05:54 -0300

    fix: arrumando o userservice

--- src/main/java/com/trabalho/api/autores_livros/services/UserService.java
+++ src/main/java/com/trabalho/api/autores_livros/services/UserService.java
@@ -1,4 +1,47 @@
 package com.trabalho.api.autores_livros.services;
 
+import com.trabalho.api.autores_livros.dto.RequestUser;
+import com.trabalho.api.autores_livros.dto.ResponseUser;
+import com.trabalho.api.autores_livros.models.User;
+import com.trabalho.api.autores_livros.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private String generateApiKey(){
+        return UUID.randomUUID().toString();
+    }
+
+    public ResponseUser createUser(RequestUser userContent){
+        if(userRepository.findByEmail(userContent.email()).isPresent()){
+            throw new RuntimeException("E-mail ja cadastrado em outro user");
+        }
+        User user = new User(userContent.email(), userContent.password());
+        user.setApiKey(generateApiKey());
+
+        User savedUser = userRepository.save(user);
+
+        return new ResponseUser(savedUser.getEmail(), savedUser.getId(), savedUser.getApiKey());
+    }
+
+    public List<ResponseUser> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new ResponseUser(
+                        user.getEmail(),
+                        user.getId(),
+                        user.getApiKey()
+                ))
+                .toList();
+    }
+
+
 }
